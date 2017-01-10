bc7f3182bfbd7587450423f6d10995965256b424
Arnold Graf <arnold.graf@gmail.com>
2017-01-10 20:02:13 +0100

    no message

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: "AIzaSyC5DJnzCQ8aNrm_8QbbI9o-JDNXprFKoR8",
-  authDomain: "arnis-test.firebaseapp.com",
-  databaseURL: "https://arnis-test.firebaseio.com",
-  storageBucket: "arnis-test.appspot.com",
-  messagingSenderId: "312934447520"
-}
-firebase.initializeApp(config)
